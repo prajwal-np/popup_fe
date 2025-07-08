360c9e3bd5f862a5ebdd6c48e1c65f8bf6c55ddf
prasanna <prasanna.shrestha@upaya.com.np>
2025-07-08 13:41:51 +0545

    ui fixes for forgot password and reset password layout

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,9 @@
+import ResetPasswordComponent from "./reset-password"
+
+const ResetPasswordPage = () => {
+   return <div>
+      <ResetPasswordComponent />
+   </div>
+}
+
+export default ResetPasswordPage;
